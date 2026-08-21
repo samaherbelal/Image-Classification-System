cd78125bcd91b111bfba7b3fb14a4cbcd6d0cc9f
samaher <samaherbelal.a@gmail.com>
2026-08-21 18:50:58 +0300

    commit 4

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -29,7 +29,6 @@ validation_dataset = tf.keras.utils.image_dataset_from_directory(
     image_size=IMG_size,
     batch_size=BATCH_SIZE,
     shuffle = False #لأننا لا نحتاج خلط بيانات التقييم
-
 )
 
 test_dataset = tf.keras.utils.image_dataset_from_directory(
@@ -45,3 +44,42 @@ class_names = train_dataset.class_names
 print("Classes: ", class_names)
 print("Number of classes: ", len(class_names))
 
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip("horizontal"), 
+    tf.keras.layers.RandomRotation(0.1), 
+    tf.keras.layers.RandomZoom(0.1) 
+])
+
+normalization_layer = tf.keras.layers.Rescaling(1.0 / 255) #لطبقة لتحويل قيم البكسل الى نطاق من 0 الى 1
+
+# دوال تعرف كيف نعالج البيانات تستقبل الصورة وتصنيفه
+def preprocess_train(images, labels):
+    images = data_augmentation(images)
+    images = normalization_layer(images)
+    return images, labels
+
+
+def preprocess_validation(images, labels):
+    images = normalization_layer(images)
+    return images, labels
+
+
+def preprocess_test(images, labels):
+    images = normalization_layer(images)
+    return images, labels
+
+
+train_data = train_dataset.map(
+    preprocess_train,
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+
+validation_data = validation_dataset.map(
+    preprocess_validation,
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+
+test_data = test_dataset.map(
+    preprocess_test,
+    num_parallel_calls=tf.data.AUTOTUNE
+)
